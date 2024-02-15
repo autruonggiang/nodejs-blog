22c4629ed6739b5600d6c4d1f974a86c4d35b644
autruonggiang <21522019@gm.uit.edu.vn>
2024-02-15 14:35:24 +0700

    feat: Install Morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/tin-tuc', (req, res) => {
     var a = 1;
